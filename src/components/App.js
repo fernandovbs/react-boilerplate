e381aca3c14e89a092b460e4a0a1113afb510d6e
Fernando Souza <fernandostorm1@gmail.com>
2018-05-13 15:41:03 -0300

    Change Hello World example

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 
 class App extends Component {
     render() {
+        const place = 'World'
         return (
             <div>
-                <h1>Starter App</h1>
+                <h1>Hello { place }!</h1>
             </div>
         )
     }
